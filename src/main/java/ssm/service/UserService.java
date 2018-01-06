1e0a69bb60af7a5c12c964f0e67e60bdec28c069
scyhssm <347966819@qq.com>
2018-01-06 14:21:18 +0800

    Initial commit

---
+++ src/main/java/ssm/service/UserService.java
@@ -0,0 +1,18 @@
+package ssm.service;
+
+
+import ssm.model.User;
+
+import java.util.List;
+
+/**
+ * Created by Zhangxq on 2016/7/15.
+ */
+public interface UserService {
+
+    List<User> getAllUser();
+
+    User getUserByPhoneOrEmail(String emailOrPhone, Short state);
+
+    User getUserById(Long userId);
+}
